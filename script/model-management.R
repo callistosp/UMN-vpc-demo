098c9cab9467feef6dde57fc3244559c5c574f9a
Samuel Callisto <samc@metrumrg.com>
2021-08-26 00:53:36 -0400

    finish writing demo

--- script/model-management.R
+++ script/model-management.R
@@ -17,7 +17,7 @@
 library(bbr)
 library(tidyverse)
 
-source("functions-model.R")
+# source("functions-model.R")
 
 
 # define model dir and load tags
@@ -26,6 +26,11 @@ MODEL_DIR <- here::here("model","nonmem")
 # bbi_init(.dir = MODEL_DIR,            # the directory to create the bbi.yaml in
 #          .nonmem_dir = "/opt/NONMEM", # location of NONMEM installation
 #          .nonmem_version = "nm74gf")  # default NONMEM version to use
+
+## update data file and rerun model
+# data <- data.table::fread(here("data","dat1.csv"), na=".")
+# data1 <- mutate(data, NUM=row_number())
+# PKPDmisc::write_nonmem(data1, here::here("data","dat1.csv"))
 
 # mod <- new_model(file.path(MODEL_DIR, "r2"))
 mod <- read_model(file.path(MODEL_DIR, "r2"))
